79ef568981045a691ccef729fa61d2bfbb2bb567
Karol <super000999888@gmail.com>
2021-03-12 14:36:15 +0800

    feat: refactor restaurantSeeder.js

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -7,33 +7,27 @@ if (process.env.NODE_ENV !== 'production') {
 const db = require('../../config/mongoose')
 
 const restaurantList = require('../../restaurant.json')
-const SEED_USER_ONE = { email: 'user1@example.com', password: '12345678' }
-const SEED_USER_TWO = {email: 'user2@example.com', password: '12345678'}
+const SEED_USER = [
+  { email: 'user1@example.com', password: '12345678' },
+  { email: 'user2@example.com', password: '12345678' }
+]
 
-function generateData (userInfo, index_one, index_two) {
-  db.once('open', () => {
-    bcrypt.genSalt(10).then(salt => bcrypt.hash(userInfo.password, salt))
-      .then(hash => {
-        return User.create({
-          email: userInfo.email,
-          password: hash
-        })
-          .then(user => {
-            const userId = user._id
-            const restaurants = restaurantList.results.slice(index_one, index_two)
-            const restaurantsForUser = restaurants.map(restaurant => {
-              restaurant.userId = userId
-              return restaurant
-            })
-            return Promise.all([Restaurant.create(restaurantsForUser)])
-          })
+db.once('open', () => {
+  SEED_USER.forEach((user, index) => {
+    bcrypt.genSalt(10).then(salt => bcrypt.hash(user.password, salt))
+      .then(hash => User.create({
+        email: user.email,
+        password: hash
+      }))
+      .then(user => {
+        const userId = user._id
+        return Promise.all(Array.from({ length: 3 }, (_, i) => Restaurant.create({
+          ...restaurantList.results[(i + (index * 3))], userId
+        })))
       })
       .then(() => {
         console.log('done.')
         process.exit()
       })
   })
-}
-
-generateData(SEED_USER_ONE, 0, 3)
-generateData(SEED_USER_TWO, 3, 6)
+})
